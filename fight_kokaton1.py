653dfcdd6d79dfc3b4282966f843ccb392f619b9
suzukishota <c0b22080ae@edu.teu.ac.jp>
2023-05-09 17:46:26 +0900

    追加機能５：gameoverの文字を出す

--- fight_kokaton1.py
+++ fight_kokaton1.py
@@ -1,13 +1,12 @@
 import random
 import sys
 import time
-import pygame
+
 import pygame as pg
+
 
 WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
-NUM_OF_BOMBS = 5  # 爆弾の数
-WHITE = (255,255,255)
 
 
 def check_bound(area: pg.Rect, obj: pg.Rect) -> tuple[bool, bool]:
@@ -44,7 +43,7 @@ class Bird:
         """
         self._img = pg.transform.flip(  # 左右反転
             pg.transform.rotozoom(  # 2倍に拡大
-                pg.image.load(f"fig/{num}.png"), 
+                pg.image.load(f"ex03/fig/{num}.png"), 
                 0, 
                 2.0), 
             True, 
@@ -53,15 +52,13 @@ class Bird:
         self._rct = self._img.get_rect()
         self._rct.center = xy
 
-    
-
     def change_img(self, num: int, screen: pg.Surface):
         """
         こうかとん画像を切り替え，画面に転送する
         引数1 num：こうかとん画像ファイル名の番号
         引数2 screen：画面Surface
         """
-        self._img = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 2.0)
+        self._img = pg.transform.rotozoom(pg.image.load(f"ex03/fig/{num}.png"), 0, 2.0)
         screen.blit(self._img, self._rct)
 
     def update(self, key_lst: list[bool], screen: pg.Surface):
@@ -84,20 +81,18 @@ class Bomb:
     """
     爆弾に関するクラス
     """
-    _colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255)]
-    _dires = [-1, 0, +1]
-    def __init__(self):
+    def __init__(self, color: tuple[int, int, int], rad: int):
         """
-        爆弾円Surfaceを生成する
+        引数に基づき爆弾円Surfaceを生成する
+        引数1 color：爆弾円の色タプル
+        引数2 rad：爆弾円の半径
         """
-        rad = random.randint(10, 50)
-        color = random.choice(Bomb._colors)
         self._img = pg.Surface((2*rad, 2*rad))
         pg.draw.circle(self._img, color, (rad, rad), rad)
         self._img.set_colorkey((0, 0, 0))
         self._rct = self._img.get_rect()
         self._rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-        self._vx, self._vy = random.choice(Bomb._dires), random.choice(Bomb._dires)
+        self._vx, self._vy = +1, +1
 
     def update(self, screen: pg.Surface):
         """
@@ -113,81 +108,33 @@ class Bomb:
         screen.blit(self._img, self._rct)
 
 
-class Beam:
-    """
-    ビームに関するクラス
-    """
-    def __init__(self, bird: Bird):
-        """
-        割愛
-        """
-        self._img = pg.transform.rotozoom(pg.image.load(f"fig/beam.png"), 0, 2.0)  # 画像surface
-        self._rct = self._img.get_rect()  # 画像surfaceに対応したrect
-        self._rct.left = bird._rct.right  # こうかとんの右側にビームの左側を合わせる
-        self._rct.centery = bird._rct.centery
-        self._vx, self._vy = +1, 0
-
-    def update(self, screen: pg.Surface):
-        """
-        ビームを速度self._vyに基づき移動させる
-        引数 screen：画面Surface
-        """
-        self._rct.move_ip(self._vx, self._vy)
-        screen.blit(self._img, self._rct)
-
-
-def draw_text(screen,x,y,text,size,col):#文字表示の関数
-        font = pygame.font.Font(None,size)
-        s = font.render(text,True,col)
-        x = x - s.get_width()/2
-        y = y - s.get_height()/2
-        screen.blit(s,[x,y])
-
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     clock = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("ex03/fig/pg_bg.jpg")
 
     bird = Bird(3, (900, 400))
-    bombs = [Bomb() for _ in range(NUM_OF_BOMBS)]
-    beam = None
+    bomb = Bomb((255, 0, 0), 10)
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                beam = Beam(bird)
-
         tmr += 1
         screen.blit(bg_img, [0, 0])
         
-        
-        for bomb in bombs:
-            bomb.update(screen)
-            if bird._rct.colliderect(bomb._rct):
-                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                bird.change_img(8, screen)
-                draw_text(screen,800,450,"GAMEOVER",300,WHITE)
-                pg.display.update()
-                time.sleep(1)
-                
-                return
-            
+        if bird._rct.colliderect(bomb._rct):
+            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+            bird.change_img(8, screen)
+            pg.display.update()
+            time.sleep(1)
+            return
+
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-
-        if beam is not None:  # ビームが存在しているとき
-            beam.update(screen)
-            for i, bomb in enumerate(bombs):
-                if beam._rct.colliderect(bomb._rct):
-                    beam = None
-                    del bombs[i]
-                    bird.change_img(6, screen)
-                    break
-
+        bomb.update(screen)
         pg.display.update()
         clock.tick(1000)
 
